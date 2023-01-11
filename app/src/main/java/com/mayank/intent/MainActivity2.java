3bd11fa6fecd99453c0c6edc6ede5fc33abd64ae
mayank4singh <mailatmausam@gmail.com>
2023-01-11 23:06:40 +0530

    first commit

---
+++ app/src/main/java/com/mayank/intent/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.mayank.intent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        btn=findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
